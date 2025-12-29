4b9e57fe3aa9518b1a1a73aa220b9b242d0a2f08
Junaidniz786 <Junaid20305@gmail.com>
2025-12-29 23:36:20 +0500

    Update PHP session ID in API request headers

--- api/tempotps.js
+++ api/tempotps.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=elri0no87kve8tfb6gc3avkukl",
+    Cookie: "PHPSESSID=329aav8hbu1fri3es58hr8nkb1",
   };
 
   let url;
