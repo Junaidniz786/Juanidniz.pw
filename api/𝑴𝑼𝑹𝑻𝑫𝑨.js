3dca290ea37f83b8296dee39d47e577c6ed8e6ce
Junaidniz786 <Junaid20305@gmail.com>
2026-01-16 00:14:58 +0500

    Update User-Agent and Cookie in headers

--- api/𝑴𝑼𝑹𝑻𝑫𝑨.js
+++ api/𝑴𝑼𝑹𝑻𝑫𝑨.js
@@ -49,12 +49,12 @@ module.exports = async (req, res) => {
 
   const headers = {
     "User-Agent":
-      "Mozilla/5.0 (Linux; Android 15; V2423 Build/AP3A.240905.015.A2_D1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.171 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 15; V2423 Build/AP3A.240905.015.A2_D1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.7499.146 Mobile Safari/537.36",
     Accept: "application/json, text/javascript, */*; q=0.01",
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=1ri6hilmtpdtdodtma9t56hg1f",
+    Cookie: "PHPSESSID=drisp2q19ptkbejc0usk1pq8av",
   };
 
   let url;
